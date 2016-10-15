6fafb1b64f676ab83f144af0753950c32f40df3f
Matt Lee <matt@reddit.com>
2016-10-15 00:39:05 -0400

    Add live updates
    Changes to any panel will instantly appear in the others.

--- editor.js
+++ editor.js
@@ -11,53 +11,72 @@ class Editor {
 
         this.txtInput.value = this.loadFromLocalStorage('txtInput.value');
         this.jsonInput.value = this.loadFromLocalStorage('jsonInput.value');
+        if (this.jsonInput.value) {
+            this.renderJSON();
+        }
 
         this.txtInput.addEventListener('input', (e) => {
             this.saveToLocalStorage('txtInput.value', this.txtInput.value);
+            this.parseTxt();
         });
 
         this.jsonInput.addEventListener('input', (e) => {
             this.saveToLocalStorage('jsonInput.value', this.jsonInput.value);
+            this.formatJSON();
         });
 
         this.txtButton.addEventListener('click', (e) => {
-            try {
-                let text = this.txtInput.value;
-                let p = new Parser();
-                p.parse(text);
-                this.jsonInput.value = JSON.stringify(p, null, 4);
-                this.saveToLocalStorage('jsonInput.value', this.jsonInput.value);
-            } catch (err) {
-                this.jsonInput.value = err.stack;
-            }
+            this.parseTxt();
             e.preventDefault();
         });
 
         this.jsonButton.addEventListener('click', (e) => {
-            try {
-                let text = this.jsonInput.value;
-                let json = JSON.parse(text);
-                let f = new Formatter();
-                f.format(json);
-                this.txtInput.value = f.output;
-                this.saveToLocalStorage('txtInput.value', this.txtInput.value);
-            } catch (err) {
-                this.txtInput.value = err.stack;
-            }
+            this.formatJSON();
             e.preventDefault();
         });
 
         this.renderButton.addEventListener('click', (e) => {
-            try {
-                let text = this.jsonInput.value;
-                let json = JSON.parse(text);
-                let r = new Renderer();
-                r.render(json, this.traceRenderer);
-            } catch (err) {
-                this.renderOutput.textContent = err.stack;
-            }
+            this.renderJSON();
             e.preventDefault();
         });
+    }
+
+    parseTxt() {
+        try {
+            let text = this.txtInput.value;
+            let p = new Parser();
+            p.parse(text);
+            this.jsonInput.value = JSON.stringify(p, null, 4);
+            this.saveToLocalStorage('jsonInput.value', this.jsonInput.value);
+            this.renderJSON();
+        } catch (err) {
+            this.jsonInput.value = err.stack;
+        }
+    }
+
+    formatJSON() {
+        try {
+            let text = this.jsonInput.value;
+            let json = JSON.parse(text);
+            let f = new Formatter();
+            f.format(json);
+            this.txtInput.value = f.output;
+            this.saveToLocalStorage('txtInput.value', this.txtInput.value);
+            this.renderJSON();
+        } catch (err) {
+            this.txtInput.value = err.stack;
+        }
+    }
+
+    renderJSON() {
+        try {
+            let text = this.jsonInput.value;
+            let json = JSON.parse(text);
+            let r = new Renderer();
+            r.render(json, this.traceRenderer);
+        } catch (err) {
+            this.renderOutput.textContent = err.stack;
+        }
     }
 
     loadFromLocalStorage(key) {
